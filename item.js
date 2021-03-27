6be3d79f903fc9fb24c117f43efbe19ad03374ad
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-27 17:21:39 -0400

    update item.js

--- item.js
+++ item.js
@@ -2,9 +2,44 @@
 const items = require("./fakeDb")
 
 class Item {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+        
+        // keep track of all items
+        items.push(this);
+    }
 
+    static findAll() {
+        return items
+    }
+    // Update found item with matching name to data 
+
+    static update(name, data){
+        let foundItem = items.find(name);
+        if (foundItem === undefined) {
+            throw{message: "Item not found", status: 404}
+        }
+        foundItem.name = data.name;
+        foundItem.price = data.price;
+
+        return foundItem;
+    }
+
+// Find and return item with matching name
+
+static find(name){
+    const foundItem = items.find(v => v.name === name);
+    if (foundItem === undefined) {
+        throw{message: "Item not found", status: 404}
+    }
+    return foundItem
+}
 }
 
+
+
+//Constructor classes 
 // its a way to pass in information dynacically
 // the 'new keyword' function
 // -creates a new empty object {}
