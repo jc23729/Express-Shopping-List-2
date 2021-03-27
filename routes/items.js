02344ab52766ecd8cb76e7619637f378caaa0ac0
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-27 18:11:02 -0400

    update item.js

--- routes/items.js
+++ routes/items.js
@@ -3,4 +3,11 @@ const express = require('express');
 
 const router = express.Router();
 
-
+/** GET / => [item, ...] */
+router.get('', (req, res, next) => {
+    try {
+      return res.json({ items: Item.findAll() });
+    } catch(err){
+      return next(err)
+    }
+  });
