3e511b13eb5fcc531588a4f276a514199d816df1
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-27 18:09:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(function(req, res, next) {
   return new ExpressError("Not Found", 404);
 });
 
+//general error handler
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
 
 
 app.get('/dogs', function(request, response) {
@@ -22,3 +31,6 @@ app.listen(3000, function(){
 }) 
 
 //get the app working
+
+
+module.exports = app
