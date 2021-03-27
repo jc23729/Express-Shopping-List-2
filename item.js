b1917c8e140d265bb75770f90988e80b36cd8424
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-27 16:35:48 -0400

    class constructor

---
+++ item.js
@@ -0,0 +1,12 @@
+/** Item in a shopping cart. */
+const items = require("./fakeDb")
+
+class Item {
+
+}
+
+
+// the 'new keyword' function
+// -creates a new empty object {}
+// -sets value of 'this' to be the new empty object
+// -calls the constructor method
