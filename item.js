2c11a6b909f0a45ab666cd24691bce71cb427c51
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-27 17:27:10 -0400

    update

--- item.js
+++ item.js
@@ -2,53 +2,53 @@
 const items = require("./fakeDb")
 
 class Item {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-        
-        // keep track of all items
-        items.push(this);
-    }
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
 
-    static findAll() {
-        return items
-    }
-    // Update found item with matching name to data 
+    // keep track of all items
+    items.push(this);
+  }
 
-    static update(name, data){
-        let foundItem = items.find(name);
-        if (foundItem === undefined) {
-            throw{message: "Item not found", status: 404}
-        }
-        foundItem.name = data.name;
-        foundItem.price = data.price;
+  static findAll(){
+    return items
+  }
+  /** Update found item with matching name to data. */
 
-        return foundItem;
-    }
-
-// Find and return item with matching name
-
-static find(name){
-    const foundItem = items.find(v => v.name === name);
+  static update(name, data) {
+    let foundItem = Item.find(name);
     if (foundItem === undefined) {
-        throw{message: "Item not found", status: 404}
+      throw {message: "Not Found", status: 404}
+    }
+    foundItem.name = data.name;
+    foundItem.price = data.price;
+
+    return foundItem;
+  }
+
+  /** Find & return item with matching name. */
+
+  static find(name) {
+    const foundItem = items.find(v => v.name === name);
+    if(foundItem === undefined){
+      throw { message: "Not Found", status: 404 }
     }
     return foundItem
-}
+  }
 
-// Remove item with matching id
+  /** Remove item with matching id. */
 
-static remove(name){
-    let foundIdx = items.findIndex(v => v.id === name);
+
+  static remove(name) {
+    let foundIdx = items.findIndex(v => v.name === name);
     if (foundIdx === -1) {
-        throw{message: "Item not found", status: 404}
+      throw {message: "Not Found", status: 404}
     }
     items.splice(foundIdx, 1);
-}
+  }
 }
 
 module.exports = Item;
-
 
 
 //Constructor classes 
---
+++ routes/items.js
@@ -0,0 +1,1 @@
+cosnt Item = require('./item');
